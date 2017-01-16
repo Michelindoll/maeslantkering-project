e341a6434b2a6b583b7b33c43129febf0aef3784
Frans <fransirgo@hotmail.com>
2017-01-16 08:55:35 +0100

    Updated db, added requirements

--- db.py
+++ db.py
@@ -4,8 +4,17 @@ from auth import dbpass, dbuser, dbhost, db
 def createSQLConnection():
     return pymysql.connect(host=dbhost, user=dbuser, password=dbpass, db=db)
 
-def SelectFromDB{
+def WriteToDB(table, field, value):
+    connection = createSQLConnection()
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO {} ({}) VALUES ({})".format(table, field, value)
+        cursor.execute(sql)
+    connection.commit()
 
-}
-
-
+def SelectFromDB(field, table, where):
+    connection = createSQLConnection()
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT {} FROM {} WHERE {}".format(field, table, where)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
