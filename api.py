9499a07f6a0cd92da6d2760e28e831fc7234a45f
Frans <fransirgo@hotmail.com>
2017-01-23 20:58:40 +0100

    Rijkswaterstaat water level API

---
+++ api.py
@@ -0,0 +1,20 @@
+import json
+try:
+    # Python 3
+    from urllib.request import urlopen, Request
+except ImportError:
+    # Fallback for Python 2
+    from urllib import urlopen, request
+
+def GetNAPMaeslantkering():
+    url = "http://www.rijkswaterstaat.nl/apps/geoservices/rwsnl/?mode=features&projecttype=waterstanden"
+    response = urlopen(url)
+    output = json.loads(response.read().decode("utf-8"))
+    for item in output['features']:
+        if item['locatienaam'] == 'Maeslantkering zeezijde N':
+            measlantkeringwaterstand = item
+            break
+    return measlantkeringwaterstand
+
+measlantkeringwaterstand = GetNAPMaeslantkering()
+print(measlantkeringwaterstand['locatienaam']+' '+measlantkeringwaterstand['waarde']+'cm NAP')
