027edb1b74eac7de4eec02b0c0c0afca78101a05
Frans <fransirgo@hotmail.com>
2017-01-23 15:51:46 +0100

    Updated with meetstation module

--- db.py
+++ db.py
@@ -4,13 +4,6 @@ from auth import dbpass, dbuser, dbhost, db
 def createSQLConnection():
     return pymysql.connect(host=dbhost, user=dbuser, password=dbpass, db=db)
 
-def WriteToDB(table, field, value):
-    connection = createSQLConnection()
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO {} ({}) VALUES ({})".format(table, field, value)
-        cursor.execute(sql)
-    connection.commit()
-
 def SelectFromDB(field, table, where):
     connection = createSQLConnection()
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
@@ -18,3 +11,10 @@ def SelectFromDB(field, table, where):
         cursor.execute(sql)
         result = cursor.fetchall()
         return result
+
+def WriteSensorDataToDB(WaterLevel, Timestamp):
+    connection = createSQLConnection()
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO SensorData (WaterLevel, Time) VALUES ({},{})".format(WaterLevel, Timestamp)
+        cursor.execute(sql)
+    connection.commit()
