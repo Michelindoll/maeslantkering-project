1505da4a45a8b9b0e1d29284b6912aeda345c26c
Michelindoll <michelwijkstra@icloud.com>
2017-01-24 18:22:08 +0100

    Commandostation voorzien van comments en de code opgeruimd

--- commandostation.py
+++ commandostation.py
@@ -1,73 +1,78 @@
 import time, zmq, threading
 
-a = 0
+mode = 0
 
-def connectionHandler():
+#Functies:
+def heartbeatHandler():
+    #Reageert op de heartbeat's van het secundaire commandostation.
+    #Als deze reactie niet ontvangen wordt wisselt het commandostation naar primaire modus.
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
     while True:
-        message = socket.recv()
+        socket.recv()
         time.sleep(0.1)
-        socket.send(b"rep")
+        socket.send(b"reply")
 
-def requestSender():
-    global a
+def heartbeatSender():
+    #Verstuurt een heartbeat naar het primaire commandostation.
+    #Als de heartbeat niet beantwoord wordt schakeld het station om naar primaire modus.
+    global mode
     context = zmq.Context()
-    print("Zoeken naar ander meetstation...")
+    #print("Zoeken naar primair contolestation...")
     socket = context.socket(zmq.REQ)
     socket.RCVTIMEO = 1000
     socket.connect("tcp://localhost:5555")
-    if a == 0 or a == 1:
+    if mode == 0 or mode == 1:
         try:
-            print(1)
             socket.send(b"req")
-            #time.sleep(0.1)
-            print(2)
             socket.recv()
-            print(3)
-            a = 1
+            mode = 1
         except:
-            a = 2
+            mode = 2
             context.term()
-        #else:
-         #   context.term()
 
 def DoorControl(Action):
-    print(5)
     if Action == 1:
         message = b'1'
     elif Action == 0:
         message = b'0'
     context = zmq.Context()
-    print("Kijk naar deur")
+    print("Verbinden met sluisaansturing...")
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5556")
-    print("Sending request …")
+    print("Verzenden van verzoek...")
     socket.send(message)
     time.sleep(1)
     message = socket.recv()
     print(message.decode("ascii"))
 
+#Controleren of het primaire commandostation actief is.
+#Als dit niet het het geval is zal deze instance als primary functioneren.
+threading.Thread(target=heartbeatSender).start()
 
-threading.Thread(target=requestSender).start()
-
-while a < 1:
+#Gun het programma een moment om te communiceren met het andere commandostation
+while mode < 1:
     time.sleep(1)
-if a == 1:
-    print("Secundaire modus wordt gestart")
-    while a == 1:
-        threading.Thread(target=requestSender).start()
-        print("Poll")
-        time.sleep(1)
-if a == 2:
-    print("Primaire modus wordt gestart")
-    threading.Thread(target=connectionHandler).start()
-    print("4")
-    time.sleep(5)
-    threading.Thread(DoorControl(1)).start()
-else:
-    print("Het programma is niet goed gestart")
 
-print("idi nahui")
+#Als het primaire commandostation actief wordt er naar secundaire modus geschakeld.
+#In deze modus wordt elke seconde een heartbeat naar het primaire commandostation verstuurd.
+#Wanneer hier geen reactie op komt schakeld het programma naar primaire modus.
+if mode == 1:
+    print("Secundaire modus wordt gestart")
+    while mode == 1:
+        threading.Thread(target=heartbeatSender).start()
+        print("Heartbeat verzonden.")
+        time.sleep(1)
+
+#Start de primaire modus. Hier wordt de beslissing genomen om de kering te sluiten als er aan de criteria voldaan wordt.
+#Tevens start de heartbeatHandler, die reacties geeft op de reacties van het secundaire controlestation
+#todo: Oppakken van de data en een beslissing maken.
+if mode == 2:
+    print("Primaire modus wordt gestart")
+    threading.Thread(target=heartbeatHandler).start()
+    #Onderliggende is alleen om te testen.
+    time.sleep(5)
+    print("Maximale waterniveau overschreden, Sluiting wordt in gang gezet.")
+    threading.Thread(DoorControl(1)).start()
 
