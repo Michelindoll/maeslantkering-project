f49312f302956b0f03775a43f63c39cd474d422b
Frans <fransirgo@hotmail.com>
2017-01-25 10:04:16 +0100

    Made the grabber more reliable.

--- datagrabber.py
+++ datagrabber.py
@@ -3,18 +3,25 @@ import db,api, time, datetime
 waterstand = 0
 tijd = 0
 while True:
-    latestdata = db.SelectLastReadingFromDB()
+    try:
+        latestdata = db.SelectLastReadingFromDB()
+    except:
+        print("Getting latest data failed")
     print("Getting data")
-    maeslantkeringdata = api.GetNAPMaeslantkering()
-    print("Got API data")
-    waterstand = maeslantkeringdata['waarde']
-    tijd = maeslantkeringdata['meettijd']
-    if tijd != latestdata[1]:
-        print("Posting data to DB")
-        db.WriteSensorDataToDB(waterstand,tijd)
-        print("Posted data to DB")
-    else:
-        print("Dubbele Waarde")
+    try:
+        maeslantkeringdata = api.GetNAPMaeslantkering()
+        print("Got API data")
+        waterstand = maeslantkeringdata['waarde']
+        tijd = maeslantkeringdata['meettijd']
+        print(tijd, latestdata[1])
+        if int(tijd) != int(latestdata[1]):
+            print("Posting data to DB")
+            db.WriteSensorDataToDB(waterstand,tijd)
+            print("Posted data to DB")
+        else:
+            print("Dubbele Waarde")
+    except:
+        print("Api call failed")
     print(str(datetime.datetime.now()))
     print("Sleeping")
     time.sleep(1200)
